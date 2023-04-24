99b751990abf5efc6f0f894dce212d09de6f4848
Simón Felipe Muñoz Osorio <51033434+WitchImage@users.noreply.github.com>
2023-04-23 21:08:08 -0500

    feat: add models types

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,4 +1,42 @@
+export interface TimeStamps {
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface Token extends Omit<TimeStamps, 'updatedAt'> {
+    ip?: string;
+    token: string;
+    expirationDate: string;
+}
+
 export interface User {
     email?: string;
-    token?: string;
+    token?: Token;
+}
+
+export interface BasePerson extends TimeStamps {
+    name: string;
+    documentType: string;
+    document: string;
+}
+
+export interface Admin extends BasePerson {
+    user: User;
+}
+
+export interface Operator extends BasePerson {
+    user: User;
+    shift: string;
+    token: Token;
+}
+
+export interface StopAndGo {
+    location: string;
+    alerts: any[];
+    vehicles: any[];
+}
+
+export interface Company extends BasePerson {
+    operators: Operator[];
+    stopAndGos: StopAndGo[];
 }
